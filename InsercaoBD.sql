b833021dbc87760848a9300cbe63db7b51732e99
GabrielRocha1031 <54642896+GabrielRocha1031@users.noreply.github.com>
2022-11-20 15:50:04 -0300

    Add files via upload

--- InsercaoBD.sql
+++ InsercaoBD.sql
@@ -2,7 +2,7 @@ INSERT INTO `PESSOAS` VALUES
 ('1','Cristian Ghyprievy','2','S',NULL),
 ('2','Joana Cabel','1','S',NULL),
 ('3','John Serial','3','S',NULL),
-('4','Michael Seven','4','N','2021-09-25');
+('4','Michael Seven','2','N','2021-09-25');
 
 INSERT INTO `CONTRATOS` VALUES
 ('1','150.00','100'),
